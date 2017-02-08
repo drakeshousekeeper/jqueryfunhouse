a97c79b785d804244f11d29c17d62e07789e072a
Syndra <syndra.j.kyddhardy@student.uncommonhs.org>
2017-02-08 22:30:36 +0000

    Changed HTML and JAVA

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-
 $(document).ready( function(){
-    $('#change-color').click(function(){
-        $('body').css("background-color","red");
+    
+    // -- NAME
+    $("#submit-name").click(function(){
+    $("#greeting").html("What up, " + $("#name").val());
     })
+    
+    // -- CHANGE COLOR
+    var colors = ["#800000", "#ADFB57", "#575bfb"]
+    $('#change-color').click(function(){
+        $('body').css("background-color", colors[Math.round(Math.random() * 2)]);
+    })
+    
+    // -- PICS
+    $(".gallery-thumb").click(function(){
+    //$("#gallery-main").attr("img", "")
+    })
+    
 })
 
